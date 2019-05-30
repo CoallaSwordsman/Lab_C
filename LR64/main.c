6a675f32864a682ea8c69d1094949209a0a70e70
Coalla_Samurai <vladyslav.voloshyn0@gmail.com>
2019-05-30 04:09:19 +0300

    'labkiii'

--- LR64/main.c
+++ LR64/main.c
@@ -2,6 +2,19 @@
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+    int b[20] = {0};
+    int sum = 0;
+    int size = 0;
+    for (int i = 0; i < sizeof (b); i++)
+    {
+        scanf("%d", &b[i]);
+        if(b[i] <= 0)
+        {
+            break;
+        }
+        sum += b[i];
+        size++;
+    }
+    float res = sum / size;
+    printf("%f", res);
 }
